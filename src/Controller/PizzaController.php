9805209cf919b0a1f7c9b72485275b6818a2dbbe
Imane Aity <imaneaity@gmail.com>
2023-02-07 16:26:17 +0100

    Page d'accueil: ajout des entités du controleur et du twig

---
+++ src/Controller/PizzaController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PizzaRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PizzaController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_pizza_home")
+     */
+    public function home(PizzaRepository $repository): Response
+    {
+        //recuperer la liste de toutes les pizzas
+        $pizzas= $repository->findAll();
+
+
+        return $this->render('pizza/home.html.twig', [
+            'pizzas' => $pizzas,
+        ]);
+    }
+}

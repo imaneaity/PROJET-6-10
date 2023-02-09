941bcb7512c8a99251228c63e2858bf4ce790a11
Imane Aity <imaneaity@gmail.com>
2023-02-09 16:22:53 +0100

    Commandes: création du DTO et du formulaire du paiement

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\OrderRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\Collection;
+use Gedmo\Mapping\Annotation\Timestampable;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity(repositoryClass=OrderRepository::class)
